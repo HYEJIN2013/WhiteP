e8267bd35008bdd512a3d69b952aa2e8b8d47ee4
yu78 <sinnakiki@gmail.com>
2016-10-07 21:39:05 +0900

    calendar
    calendar.xml

---
+++ a30c348629ba1547e5e3-7c98465286bc725f8ff870909e34987daedaf70b/CalendarView.java
@@ -0,0 +1,157 @@
+package com.examples.android.calendar;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CalendarView extends Activity {
+
+	public Calendar month, itemmonth;// calendar instances.
+
+	public CalendarAdapter adapter;// adapter instance
+	public Handler handler;// for grabbing some event values for showing the dot
+							// marker.
+	public ArrayList<String> items; // container to store calendar items which
+									// needs showing the event marker
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.calendar);
+
+		month = Calendar.getInstance();
+		itemmonth = (Calendar) month.clone();
+
+		items = new ArrayList<String>();
+		adapter = new CalendarAdapter(this, month);
+
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.setAdapter(adapter);
+
+		handler = new Handler();
+		handler.post(calendarUpdater);
+
+		TextView title = (TextView) findViewById(R.id.title);
+		title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+
+		RelativeLayout previous = (RelativeLayout) findViewById(R.id.previous);
+
+		previous.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setPreviousMonth();
+				refreshCalendar();
+			}
+		});
+
+		RelativeLayout next = (RelativeLayout) findViewById(R.id.next);
+		next.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				setNextMonth();
+				refreshCalendar();
+
+			}
+		});
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,
+					int position, long id) {
+
+				((CalendarAdapter) parent.getAdapter()).setSelected(v);
+				String selectedGridDate = CalendarAdapter.dayString
+						.get(position);
+				String[] separatedTime = selectedGridDate.split("-");
+				String gridvalueString = separatedTime[2].replaceFirst("^0*",
+						"");// taking last part of date. ie; 2 from 2012-12-02.
+				int gridvalue = Integer.parseInt(gridvalueString);
+				// navigate to next or previous month on clicking offdays.
+				if ((gridvalue > 10) && (position < 8)) {
+					setPreviousMonth();
+					refreshCalendar();
+				} else if ((gridvalue < 7) && (position > 28)) {
+					setNextMonth();
+					refreshCalendar();
+				}
+				((CalendarAdapter) parent.getAdapter()).setSelected(v);
+
+				showToast(selectedGridDate);
+
+			}
+		});
+	}
+
+	protected void setNextMonth() {
+		if (month.get(Calendar.MONTH) == month.getActualMaximum(Calendar.MONTH)) {
+			month.set((month.get(Calendar.YEAR) + 1),
+					month.getActualMinimum(Calendar.MONTH), 1);
+		} else {
+			month.set(Calendar.MONTH, month.get(Calendar.MONTH) + 1);
+		}
+
+	}
+
+	protected void setPreviousMonth() {
+		if (month.get(Calendar.MONTH) == month.getActualMinimum(Calendar.MONTH)) {
+			month.set((month.get(Calendar.YEAR) - 1),
+					month.getActualMaximum(Calendar.MONTH), 1);
+		} else {
+			month.set(Calendar.MONTH, month.get(Calendar.MONTH) - 1);
+		}
+
+	}
+
+	protected void showToast(String string) {
+		Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+
+	}
+
+	public void refreshCalendar() {
+		TextView title = (TextView) findViewById(R.id.title);
+
+		adapter.refreshDays();
+		adapter.notifyDataSetChanged();
+		handler.post(calendarUpdater); // generate some calendar items
+
+		title.setText(android.text.format.DateFormat.format("MMMM yyyy", month));
+	}
+
+	public Runnable calendarUpdater = new Runnable() {
+
+		@Override
+		public void run() {
+			items.clear();
+
+			// Print dates of the current week
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+			String itemvalue;
+			for (int i = 0; i < 7; i++) {
+				itemvalue = df.format(itemmonth.getTime());
+				itemmonth.add(Calendar.DATE, 1);
+				items.add("2012-09-12");
+				items.add("2012-10-07");
+				items.add("2012-10-15");
+				items.add("2012-10-20");
+				items.add("2012-11-30");
+				items.add("2012-11-28");
+			}
+
+			adapter.setItems(items);
+			adapter.notifyDataSetChanged();
+		}
+	};
+}
