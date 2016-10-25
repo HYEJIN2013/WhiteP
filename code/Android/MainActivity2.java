afb23204e8a96ba39a2474697106d77b4bf566ce
yu78 <sinnakiki@gmail.com>
2016-10-25 22:29:42 +0900

    Add files via upload
    Integrating Google Maps Version 2 using Google Play Services 1. https://developer.android.com/google/play-services/maps.html 2. https://developers.google.com/maps/documentation/android/start#get-key 3. https://developer.android.com/google/play-services/setup.html

---
+++ code/Android/MainActivity2.java
@@ -0,0 +1,8 @@
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
