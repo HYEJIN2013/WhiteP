2268130906a16e6de4249d5ef21f57eca60b1846
yu78 <sinnakiki@gmail.com>
2016-10-30 20:27:05 +0900

    Add files via upload
    ChatMessage

---
+++ code/java/ChatMessage.java
@@ -0,0 +1,14 @@
+package com.google.firebase.zerotoapp;
+
+public class ChatMessage {
+    public String name;
+    public String message;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+}
