47575474085bd6a967c5132e81a6d2dfc03988b0
HYEJIN2013 <sot5464@gmail.com>
2016-11-08 23:10:58 +0900

    Create MainActivity.java

---
+++ code/Android/chat000/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.kh_jin.chat000;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textField;
+    private Button button;
+    private TextView textView;
+    private Socket client;
+    private PrintWriter printwriter;
+    private BufferedReader bufferedReader;
+
+    private String CHAT_SERVER_IP = "10.0.2.2";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textField = (EditText) findViewById(R.id.editText1);
+        button = (Button) findViewById(R.id.button1);
+        textView = (TextView) findViewById(R.id.textView1);
+
+        ChatOperator chatOperator = new ChatOperator();
+        chatOperator.execute();
+    }
+    private class ChatOperator extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            try {
+                client = new Socket(CHAT_SERVER_IP, 4444); // Creating the server socket.
+
+                if (client != null) {
+                    printwriter = new PrintWriter(client.getOutputStream(), true);
+                    InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                } else {
+                    System.out.println("Server has not bean started on port 4444.");
+                }
+            } catch (UnknownHostException e) {
+                System.out.println("Faild to connect server " + CHAT_SERVER_IP);
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("Faild to connect server " + CHAT_SERVER_IP);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        /**
+         * Following method is executed at the end of doInBackground method.
+         */
+        @Override
+        protected void onPostExecute(Void result) {
+            button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    final Sender messageSender = new Sender(); // Initialize chat sender AsyncTask.
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                        messageSender.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    } else {
+                        messageSender.execute();
+                    }
+                }
+            });
+
+            Receiver receiver = new Receiver(); // Initialize chat receiver AsyncTask.
+            receiver.execute();
+
+        }
+
+    }
+    private class Receiver extends AsyncTask<Void, Void, Void> {
+
+        private String message;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            while (true) {
+                try {
+
+                    if (bufferedReader.ready()) {
+                        message = bufferedReader.readLine();
+                        publishProgress(null);
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ie) {
+                }
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            textView.append("Server: " + message + "\n");
+        }
+
+    }
+    private class Sender extends AsyncTask<Void, Void, Void> {
+
+        private String message = textField.getText().toString();
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            printwriter.write(message + "\n");
+            printwriter.flush();
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textField.setText(""); // Clear the chat box
+            textView.append("Client: " + message + "\n");
+        }
+    }
+
+}
