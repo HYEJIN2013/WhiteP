cf697298f31d265b6bf008fb031db9a0ba286e3d
lukakim <01020238800@hanmail.net>
2016-12-06 20:37:05 +0900

    Add files via upload

--- code/list/ArrayList.h
+++ code/list/ArrayList.h
@@ -1,12 +1,16 @@
 #ifndef __ARRAY_LIST_H__
 #define __ARRAY_LIST_H__
 
+#include "NameCard.h"
+
 #define TRUE	1
 #define FALSE	0
 
 /*** ArrayList¿« ¡§¿« ****/
 #define LIST_LEN	100
-typedef int LData;
+
+//typedef int LData;
+typedef NameCard * LData;
 
 typedef struct __ArrayList
 {
