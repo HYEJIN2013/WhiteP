8e60c858ef46749be08665c8f4ee605964a15449
HYEJIN2013 <sot5464@gmail.com>
2016-12-06 23:26:37 +0900

    Create quicksort_N_Lomuto.py

---
+++ code/sort/quicksort_N_Lomuto.py
@@ -0,0 +1,22 @@
+def partition(A, p, r):
+    x = A[r]
+    i = p - 1
+    for j in range(p, r):
+        if A[j] <= x:
+            i += 1
+            A[i], A[j] = A[j], A[i]
+    A[i+1], A[r] = A[r], A[i+1]
+    return i + 1
+
+
+def quicksort(A, p, r):
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q - 1)
+        quicksort(A, q + 1, r)
+
+
+from random import randrange
+seq = [randrange(1000) for i in range(100000)]
+import cProfile
+cProfile.run('quicksort(seq, 0, len(seq) - 1)')
