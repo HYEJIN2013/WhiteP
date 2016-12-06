1a182e9f3698f56ec2514a41d0f722c18c2b499c
HYEJIN2013 <sot5464@gmail.com>
2016-12-06 23:27:20 +0900

    Create a.py

---
+++ code/sort/a.py
@@ -0,0 +1,8 @@
+def sort_numbers(s):
+    for i in range(1, len(s)):
+        val = s[i]
+        j = i - 1
+        while (j >= 0) and (s[j] > val):
+            s[j+1] = s[j]
+            j = j - 1
+        s[j+1] = val
