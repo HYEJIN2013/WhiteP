82af7ae5961dbd2e1ef110c316b8b66179be325e
yu78 <sinnakiki@gmail.com>
2016-12-07 01:31:58 +0900

    Add files via upload
    chat

---
+++ code/java/SampleThread.java
@@ -0,0 +1,17 @@
+package tiy.networking;
+
+import java.net.Socket;
+
+/**
+ * Created by Corey Shaw on 4/27/2016.
+ */
+public class SampleThread implements Runnable {
+
+    public Socket clientSocket;
+    public void run() {
+    }
+}
+
+
+
+
