93baade255442ea9a7e0e4e0ebfb9cf4d160d622
yu78 <sinnakiki@gmail.com>
2016-12-07 01:22:41 +0900

    Add files via upload
    chat

---
+++ code/java/big01_Message.java
@@ -0,0 +1,29 @@
+package com.javarush.test.level30.lesson15.big01;
+
+import java.io.Serializable;
+
+/**
+ * Created by user-pc on 20.05.2016.
+ */
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
