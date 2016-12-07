601773742c4c873d95be1583f22baff128b76cf1
lukakim <01020238800@hanmail.net>
2016-12-07 12:51:48 +0900

    Add files via upload

--- code/sort/QuickSort.c
+++ code/sort/QuickSort.c
@@ -14,23 +14,21 @@ int Partition(int arr[], int left, int right)
 	int low = left+1;
 	int high = right;
 
-	printf("피벗: %d \n", pivot);
-
 	while(low <= high)    // 교차되지 않을 때까지 반복
 	{	
-		/*
 		while(pivot > arr[low])
 			low++;
 
 		while(pivot < arr[high])
 			high--;
-		*/
-
+		
+		/*
 		while(pivot >= arr[low] && low <= right)
 			low++;
 
 		while(pivot <= arr[high] && high >= (left+1))
 			high--;
+		*/
 
 		if(low <= high)    // 교차되지 않은 상태라면 Swap 실행
 			Swap(arr, low, high);    // low와 high가 가리키는 대상 교환
@@ -42,7 +40,7 @@ int Partition(int arr[], int left, int right)
 
 void QuickSort(int arr[], int left, int right)
 {
-	if(left < right)
+	if(left <= right)
 	{
 		int pivot = Partition(arr, left, right);    // 둘로 나눠서 
 		QuickSort(arr, left, pivot-1);    // 왼쪽 영역을 정렬
@@ -52,7 +50,8 @@ void QuickSort(int arr[], int left, int right)
 
 int main(void)
 {
-	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+//	int arr[7] = {3, 2, 4, 1, 7, 6, 5};
+	int arr[3] = {3, 3, 3};
 
 	int len = sizeof(arr) / sizeof(int);
 	int i;
