7e2e56ec3f173abb0e1196e88c17ae964b096558
lukakim <01020238800@hanmail.net>
2016-12-07 12:56:13 +0900

    Add files via upload

--- code/etc/Person.h
+++ code/etc/Person.h
@@ -5,13 +5,16 @@
 
 typedef struct _person
 {
-	int ssn;				// 주민등록번호 
-	char name[STR_LEN];		// 이름
-	char addr[STR_LEN];		// 주소
+	int ssn;    // 주민등록번호 
+	char name[STR_LEN];    // 이름
+	char addr[STR_LEN];    // 주소
 } Person;
 
+
 int GetSSN(Person * p);
+
 void ShowPerInfo(Person * p);
+
 Person * MakePersonData(int ssn, char * name, char * addr);
 
 #endif
