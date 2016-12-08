0b594e6d3094b0d9525fdbcab648899a2c0da8bb
yu78 <sinnakiki@gmail.com>
2016-12-08 21:32:09 +0900

    Add files via upload
    chat

---
+++ code/Android/src_ChatRoom.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Nick on 08.08.2016.
+ */
+public class ChatRoom {
+    private String chrName;
+
+    public ChatRoom(String chrName) {
+        this.chrName = chrName;
+    }
+
+    public String getChrName() {
+        return chrName;
+    }
+
+    public void setChrName(String chrName) {
+        this.chrName = chrName;
+    }
+}
