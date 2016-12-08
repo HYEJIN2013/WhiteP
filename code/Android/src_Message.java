24877be0f5db15a6224794009a17f4d657db9574
yu78 <sinnakiki@gmail.com>
2016-12-08 21:33:14 +0900

    Add files via upload
    chat

---
+++ code/Android/src_Message.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Date date = new Date();
+	private String from;
+	private String to="all";
+	private String text;
+	
+	public String toJSON() {
+		Gson gson = new GsonBuilder().create();
+		return gson.toJson(this);
+	}
+	
+	public static Message fromJSON(String s) {
+		Gson gson = new GsonBuilder().create();
+		return gson.fromJson(s, Message.class);
+	}
+	
+	@Override
+	public String toString() {
+		return new StringBuilder().append("[").append(date.toString())
+				.append(", From: ").append(from).append(", To: ").append(to)
+				.append("] ").append(text).toString();
+	}
+
+	public int send(String url,String id) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+		conn.setRequestProperty("id",id);
+		conn.setRequestMethod("POST");
+		conn.setDoOutput(true);
+	
+		OutputStream os = conn.getOutputStream();
+		try {
+			String json = toJSON();
+			os.write(json.getBytes());
+			
+			return conn.getResponseCode();
+		} finally {
+			os.close();
+		}
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
