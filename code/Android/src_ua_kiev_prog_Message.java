584d4a02a6c6569f4d36b1a3e6fbfdb700301748
yu78 <sinnakiki@gmail.com>
2016-12-08 21:55:30 +0900

    Add files via upload
    chat

--- code/Android/src_ua_kiev_prog_Message.java
+++ code/Android/src_ua_kiev_prog_Message.java
@@ -10,86 +10,86 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Message implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private Date date = new Date();
-	private String from;
-	private String to;
-	private String text;
-	private String room;
-	
-	public String toJSON() {
-		Gson gson = new GsonBuilder().create();
-		return gson.toJson(this);
-	}
-	
-	public static Message fromJSON(String s) {
-		Gson gson = new GsonBuilder().create();
-		return gson.fromJson(s, Message.class);
-	}
-	
-	@Override
-	public String toString() {
-		return new StringBuilder().append("[").append(date.toString())
-				.append(", From: ").append(from).append(", To: ").append(to).append(" at ").append((room.isEmpty())?" main chat":room)
-				.append("] ").append(text).toString();
-	}
+    private Date date = new Date();
+    private String from;
+    private String to;
+    private String text;
+    private String room;
 
-	public int send(String url) throws IOException {
-		URL obj = new URL(url);
-		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
-		
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-	
-		OutputStream os = conn.getOutputStream();
-		try {
-			String json = toJSON();
-			os.write(json.getBytes());
-			
-			return conn.getResponseCode();
-		} finally {
-			os.close();
-		}
-	}
-	
-	public Date getDate() {
-		return date;
-	}
+    public String toJSON() {
+        Gson gson = new GsonBuilder().create();
+        return gson.toJson(this);
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public static Message fromJSON(String s) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(s, Message.class);
+    }
 
-	public String getFrom() {
-		return from;
-	}
+    @Override
+    public String toString() {
+        return new StringBuilder().append("[").append(date.toString())
+                .append(", From: ").append(from).append(", To: ").append(to)
+                .append("] ").append(text).toString();
+    }
 
-	public void setFrom(String from) {
-		this.from = from;
-	}
+    public int send(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 
-	public String getTo() {
-		return to;
-	}
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+        OutputStream os = conn.getOutputStream();
+        try {
+            String json = toJSON();
+            os.write(json.getBytes());
 
-	public String getText() {
-		return text;
-	}
+            return conn.getResponseCode();
+        } finally {
+            os.close();
+        }
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public String getRoom() {
-		return room;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public void setRoom(String room) {
-		this.room = room;
-	}
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
