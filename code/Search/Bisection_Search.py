ed333e22326c22986bb172a0d8cff5a1d6ce6eae
HYEJIN2013 <sot5464@gmail.com>
2016-12-09 00:46:37 +0900

    Create Bisection_Search.py

---
+++ code/Search/Bisection_Search.py
@@ -0,0 +1,16 @@
+x = 1234567
+epsilon = 0.01
+numGuesses = 0 
+low = 0.0 
+high = x 
+ans = (high + low)/2.0
+while abs(ans**2 - x) >= epsilon:
+  print('low = ' + str(low) + ' high = ' + str(high) + ' ans = ' + str(ans))
+  numGuesses += 1
+  if ans**2 < x:
+    low = ans 
+  else:
+    high = ans 
+  ans = (high + low)/2.0
+print('numGuesses = ' + str(numGuesses))
+print(str(ans) + ' is close to square root of ' + str(x))
