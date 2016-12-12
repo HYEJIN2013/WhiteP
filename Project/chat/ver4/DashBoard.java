04f5c15b17d82caa6a53f06d55661f6671c40966
HYEJIN2013 <sot5464@gmail.com>
2016-12-12 23:45:04 +0900

    Create DashBoard.java

---
+++ Project/chat/ver4/DashBoard.java
@@ -0,0 +1,95 @@
+package com.example.user.chat_oss;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashBoard extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_first;
+    private TextView txtWelcome;
+    private EditText input_new_password;
+    private Button btnChangePass,btnLogout,btnStartChatting;
+    private RelativeLayout activity_dashboard;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        //View
+        txtWelcome = (TextView)findViewById(R.id.dashboard_welcome);
+        input_new_password = (EditText)findViewById(R.id.dashboard_new_password);
+        btnChangePass = (Button)findViewById(R.id.dashboard_btn_change_pass);
+        btnLogout = (Button) findViewById(R.id.dashboard_btn_logout);
+        btnStartChatting = (Button)findViewById(R.id.dashboard_btn_start_chat);
+        activity_dashboard = (RelativeLayout)findViewById(R.id.activity_dash_board);
+        btn_first = (Button)findViewById(R.id.btn_first);
+
+        btnChangePass.setOnClickListener(this);
+        btnLogout.setOnClickListener(this);
+        btnStartChatting.setOnClickListener(this);
+
+        //Init Firebase
+        auth = FirebaseAuth.getInstance();
+
+        //Session check
+        if(auth.getCurrentUser() != null)
+            txtWelcome.setText("Welcome ,"+auth.getCurrentUser().getEmail());
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.dashboard_btn_change_pass)
+            changePassword(input_new_password.getText().toString());
+        else if(view.getId() == R.id.dashboard_btn_logout)
+            logoutUser();
+        if(view.getId() == R.id.dashboard_btn_start_chat)
+        {
+            Intent intent =new Intent(getApplicationContext(),AddRoom.class);
+            startActivity(intent);
+        }
+    }
+
+
+
+    private void logoutUser() {
+        auth.signOut();
+        if(auth.getCurrentUser() == null)
+        {
+            startActivity(new Intent(DashBoard.this,LogIn.class));
+            finish();
+        }
+    }
+
+    private void changePassword(String newPassword) {
+        FirebaseUser user = auth.getCurrentUser();
+        user.updatePassword(newPassword).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful())
+                {
+                    Snackbar snackBar = Snackbar.make(activity_dashboard,"Password changed",Snackbar.LENGTH_SHORT);
+                    snackBar.show();
+                }
+            }
+        });
+    }
+}
