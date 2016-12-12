8bf36e5854514ccd9ab0fe2655374c0d79902df4
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 03:52:36 +0900

    Create Trees_dfs_dtree.py

---
+++ code/Tree/Trees_dfs_dtree.py
@@ -0,0 +1,20 @@
+def DFSDTree(root, valueFcn, constraintFcn):
+    stack = [root]
+    best = None
+    visited = 0
+    while len(stack) > 0:
+        visited += 1
+        if constraintFcn(stack[0].getValue()):
+            if best == None:
+                best = stack[0]
+            elif valueFcn(stack[0].getValue()) > valueFcn(best.getValue()):
+                best = stack[0]
+            temp = stack.pop(0)
+            if temp.getRightBranch():
+                stack.insert(0, temp.getRightBranch())
+            if temp.getLeftBranch():
+                stack.insert(0, temp.getLeftBranch())
+        else:
+            stack.pop(0)
+    print 'visited', visited
+    return best
