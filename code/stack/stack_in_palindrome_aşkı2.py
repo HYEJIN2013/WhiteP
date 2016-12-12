907db7269e271bc5b2f3b412992d2e7e28a75fa6
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 03:03:23 +0900

    Create stack_in_palindrome_aşkı2.py

---
+++ code/stack/stack_in_palindrome_aşkı2.py
@@ -0,0 +1,20 @@
+class Stack
+    def _init_ (self):
+        self.items=[]
+    
+    def isEmpty(self):
+        return self.items==[]
+        
+    def push(self,item):
+        self.items.append(item)
+        
+    def pop(self):
+        return self.items.pop()
+        
+    def peek(self):
+        return self.items[len(self.items)-1]
+        
+    def size(self)
+        return len(self.items)
+        
+        
