41e4465fc1d2f61a1d16980137ec227763e0df76
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 22:52:47 +0900

    Create DATABASE_MULTIPLE.py

---
+++ code/database/DATABASE_MULTIPLE.py
@@ -0,0 +1,22 @@
+DATABASE_MULTIPLE = dict(
+    principal = dict(
+        name = 'com-ak-proc3ss-local',
+        user = 'root',
+        pasw = 'root',
+        host = '127.0.0.1',
+        port = 27017,
+        conn = 100,
+        areq = True,
+        ugls = False          
+    ),
+    users = dict(
+        name = 'com-ak-proc3ss-local-users',
+        user = 'root',
+        pasw = 'root',
+        host = '127.0.0.1',
+        port = 27017,
+        conn = 100,
+        areq = True,
+        ugls = False          
+    ),                        
+)
