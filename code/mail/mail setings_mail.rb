d1175b8de1a5e9a8a1ddbad4c72ae74dcd221fe5
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 23:58:12 +0900

    Create mail setings_mail.rb

---
+++ code/mail/mail setings_mail.rb
@@ -0,0 +1,12 @@
+#Mails Settings
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'username',
+    :password             => 'password',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
