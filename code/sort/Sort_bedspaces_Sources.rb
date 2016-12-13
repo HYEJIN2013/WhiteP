55c00d6f47ca278c25d7855726228fabfbfe0c63
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 23:20:32 +0900

    Create Sort_bedspaces_Sources.rb

---
+++ code/sort/Sort_bedspaces_Sources.rb
@@ -0,0 +1,132 @@
+> RoomPlace.all
+RoomPlace Load (0.3ms)  SELECT `room_places`.* FROM `room_places` 
+=> [#<RoomPlace id: 34310323, room_id: 241077565, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 35109118, room_id: 309249598, number: "3", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 64344783, room_id: 192280445, number: "3", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 116842531, room_id: 310159049, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 120883732, room_id: 191338378, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 141892820, room_id: 352518693, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 163018469, room_id: 202912614, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 180380229, room_id: 931280280, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 200394942, room_id: 657809575, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 223903372, room_id: 658319371, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 252485680, room_id: 991382367, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 280897377, room_id: 202912614, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 293358956, room_id: 352518693, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 318281994, room_id: 657809575, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 339955661, room_id: 272242850, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 340598655, room_id: 990733066, number: "3", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 361723212, room_id: 571888203, number: "3", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 369488276, room_id: 991382367, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 453171467, room_id: 241077565, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 507370416, room_id: 191338378, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 536850843, room_id: 310159049, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 550435720, room_id: 154861964, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 551328639, room_id: 1044132127, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 579356127, room_id: 571888203, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 580527400, room_id: 353460962, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 592064494, room_id: 990733066, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 592989969, room_id: 202003361, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 645213519, room_id: 1043626419, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 645627393, room_id: 352518693, number: "3", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 691321737, room_id: 542174226, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 728892882, room_id: 192790485, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 739891671, room_id: 309249598, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 769373152, room_id: 192280445, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 808422090, room_id: 391634057, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 886235740, room_id: 192280445, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 890275951, room_id: 309249598, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 969386560, room_id: 154861964, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 970312391, room_id: 1044132127, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 977501778, room_id: 990733066, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 978394797, room_id: 202003361, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 998315111, room_id: 571888203, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 999519392, room_id: 353460962, number: "2", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 1026073175, room_id: 572398311, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">,
+ #<RoomPlace id: 1064180750, room_id: 154356006, number: "1", created_at: "2013-11-19 09:27:22", updated_at: "2013-11-19 09:27:22">]
+
+> Room.all
+  Room Load (0.6ms)  SELECT `rooms`.* FROM `rooms` 
+=> [#<Room id: 154356006, building_floor_id: 248843309, roominess: 1, number: "25", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 154861964, building_floor_id: 1022430802, roominess: 2, number: "13", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 191338378, building_floor_id: 318730113, roominess: 2, number: "36", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 192280445, building_floor_id: 637034474, roominess: 3, number: "25", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 192790485, building_floor_id: 400317841, roominess: 1, number: "13", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 202003361, building_floor_id: 318730113, roominess: 2, number: "32", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 202912614, building_floor_id: 637034474, roominess: 2, number: "21", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 241077565, building_floor_id: 248843309, roominess: 2, number: "21", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 272242850, building_floor_id: 248843309, roominess: 1, number: "26", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 309249598, building_floor_id: 318730113, roominess: 3, number: "35", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 310159049, building_floor_id: 637034474, roominess: 2, number: "26", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 352518693, building_floor_id: 318730113, roominess: 3, number: "31", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 353460962, building_floor_id: 637034474, roominess: 2, number: "22", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 391634057, building_floor_id: 248843309, roominess: 1, number: "22", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 542174226, building_floor_id: 248843309, roominess: 1, number: "23", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 571888203, building_floor_id: 637034474, roominess: 3, number: "23", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 572398311, building_floor_id: 400317841, roominess: 1, number: "15", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 657809575, building_floor_id: 1022430802, roominess: 2, number: "11", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 658319371, building_floor_id: 248843309, roominess: 1, number: "27", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 931280280, building_floor_id: 248843309, roominess: 1, number: "28", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 990733066, building_floor_id: 318730113, roominess: 3, number: "33", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 991382367, building_floor_id: 400317841, roominess: 2, number: "16", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 1043626419, building_floor_id: 248843309, roominess: 1, number: "24", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<Room id: 1044132127, building_floor_id: 1022430802, roominess: 2, number: "12", created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">]
+
+> BuildingFloor.all
+  SQL (1.0ms)  SELECT `building_floors`.`id` AS t0_r0, `building_floors`.`building_id` AS t0_r1, `building_floors`.`number` AS t0_r2, `building_floors`.`created_at` AS t0_r3, `building_floors`.`updated_at` AS t0_r4, `buildings`.`id` AS t1_r0, `buildings`.`number` AS t1_r1, `buildings`.`created_at` AS t1_r2, `buildings`.`updated_at` AS t1_r3 FROM `building_floors` LEFT OUTER JOIN `buildings` ON `buildings`.`id` = `building_floors`.`building_id` ORDER BY `buildings`.`number`, `building_floors`.`number`
+=> [#<BuildingFloor id: 1022430802, building_id: 778106267, number: 1, created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<BuildingFloor id: 637034474, building_id: 778106267, number: 2, created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<BuildingFloor id: 318730113, building_id: 778106267, number: 3, created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<BuildingFloor id: 400317841, building_id: 929670179, number: 1, created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">,
+ #<BuildingFloor id: 248843309, building_id: 929670179, number: 2, created_at: "2013-11-19 09:27:20", updated_at: "2013-11-19 09:27:20">]
+ 
+> Building.all
+  Building Load (0.5ms)  SELECT `buildings`.* FROM `buildings` 
+=> [#<Building id: 778106267, number: "1", created_at: "2013-11-19 09:27:21", updated_at: "2013-11-19 09:27:21">,
+ #<Building id: 929670179, number: "2", created_at: "2013-11-19 09:27:21", updated_at: "2013-11-19 09:27:21">]
+
+> arr=[];RoomPlace.all.each{|room| arr << room.to_s};puts arr.sort!;
+Корпус 1, Этаж 1, Комната 11, место № 1
+Корпус 1, Этаж 1, Комната 11, место № 2
+Корпус 1, Этаж 1, Комната 12, место № 1
+Корпус 1, Этаж 1, Комната 12, место № 2
+Корпус 1, Этаж 1, Комната 13, место № 1
+Корпус 1, Этаж 1, Комната 13, место № 2
+Корпус 1, Этаж 2, Комната 21, место № 1
+Корпус 1, Этаж 2, Комната 21, место № 2
+Корпус 1, Этаж 2, Комната 22, место № 1
+Корпус 1, Этаж 2, Комната 22, место № 2
+Корпус 1, Этаж 2, Комната 23, место № 1
+Корпус 1, Этаж 2, Комната 23, место № 2
+Корпус 1, Этаж 2, Комната 23, место № 3
+Корпус 1, Этаж 2, Комната 25, место № 1
+Корпус 1, Этаж 2, Комната 25, место № 2
+Корпус 1, Этаж 2, Комната 25, место № 3
+Корпус 1, Этаж 2, Комната 26, место № 1
+Корпус 1, Этаж 2, Комната 26, место № 2
+Корпус 1, Этаж 3, Комната 31, место № 1
+Корпус 1, Этаж 3, Комната 31, место № 2
+Корпус 1, Этаж 3, Комната 31, место № 3
+Корпус 1, Этаж 3, Комната 32, место № 1
+Корпус 1, Этаж 3, Комната 32, место № 2
+Корпус 1, Этаж 3, Комната 33, место № 1
+Корпус 1, Этаж 3, Комната 33, место № 2
+Корпус 1, Этаж 3, Комната 33, место № 3
+Корпус 1, Этаж 3, Комната 35, место № 1
+Корпус 1, Этаж 3, Комната 35, место № 2
+Корпус 1, Этаж 3, Комната 35, место № 3
+Корпус 1, Этаж 3, Комната 36, место № 1
+Корпус 1, Этаж 3, Комната 36, место № 2
+Корпус 2, Этаж 1, Комната 13, место № 1
+Корпус 2, Этаж 1, Комната 15, место № 1
+Корпус 2, Этаж 1, Комната 16, место № 1
+Корпус 2, Этаж 1, Комната 16, место № 2
+Корпус 2, Этаж 2, Комната 21, место № 1
+Корпус 2, Этаж 2, Комната 21, место № 2
+Корпус 2, Этаж 2, Комната 22, место № 1
+Корпус 2, Этаж 2, Комната 23, место № 1
+Корпус 2, Этаж 2, Комната 24, место № 1
+Корпус 2, Этаж 2, Комната 25, место № 1
+Корпус 2, Этаж 2, Комната 26, место № 1
+Корпус 2, Этаж 2, Комната 27, место № 1
+Корпус 2, Этаж 2, Комната 28, место № 1
