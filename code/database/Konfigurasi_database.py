04d2495e1ccfbdb7c26249f3ef0194f38fb05405
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 22:53:09 +0900

    Create Konfigurasi_database.py

---
+++ code/database/Konfigurasi_database.py
@@ -0,0 +1,15 @@
+import dj_database_url
+if dj_database_url.config():
+    DATABASES = {}  # error handling for no key
+    DATABASES['default'] = dj_database_url.config()
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'dev.db',                      # Or path to database file if using sqlite3.
+            'USER': '',                      # Not used with sqlite3.
+            'PASSWORD': '',                  # Not used with sqlite3.
+            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        }
+    }
