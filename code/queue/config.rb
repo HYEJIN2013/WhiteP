7f52614cf5e70d9096dd62a72167d0be0eaf65b0
HYEJIN2013 <sot5464@gmail.com>
2016-12-13 23:36:36 +0900

    Create config.rb

---
+++ code/queue/config.rb
@@ -0,0 +1,17 @@
+N_ITEMS = 10000
+QUEUE = 'rb_test_queue1'
+
+IRON_HOST = "mq-aws-us-east-1.iron.io"
+IRON_TOKEN = 'xxxx'
+IRON_PROJECT_ID = 'xxxx'
+IRON_HTTP_PORT = 8080
+IRON_BEANSTALKD_PORT = 11300
+
+BEANSTALKD_HOST = 'localhost'
+BEANSTALKD_PORT = 11300
+
+AWS_ACCESS_KEY_ID = 'xxxx'
+AWS_SECRET_ACCESS_KEY = 'xxxx'
+
+RABBITMQ_HOST = 'localhost'
+RABBITMQ_PORT = 5672
