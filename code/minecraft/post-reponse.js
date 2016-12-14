4a030572db7f57aa77c2bf1505ab52bd1dcc2355
Hiion <jyshs0930@naver.com>
2016-12-14 23:15:03 +0900

    Create post-reponse.js

---
+++ code/minecraft/post-reponse.js
@@ -0,0 +1,3 @@
+/** *  * You should be receiving all the tweets that contain Minecraft *  * */
+{  "meta": {    "total": 1,    "count": 1,    "offset": 0,    "error": null  },  "data": [    {      "tweets": [        {          "id": 2,          "user_id": 1,          "body": "minecraft",          "created_at": "2016-05-22T02:25:47.979Z",          "updated_at": "2016-05-22T02:25:47.979Z"        },        {          "id": 3,          "user_id": 1,          "body": "Minecraft",          "created_at": "2016-05-22T02:25:52.454Z",          "updated_at": "2016-05-22T02:25:52.454Z"        }      ]    }  ]
+}
